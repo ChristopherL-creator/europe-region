767dc82fc19efa080b83822b45ecba40b2030aa4
ChristopherL-creator <101825137+ChristopherL-creator@users.noreply.github.com>
2022-09-29 11:26:25 +0200

    first commit

---
+++ src/app/components/region-list-component/region-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Region } from 'src/app/models/region';
+import { RegionsService } from 'src/app/services/region-service/regions.service';
+
+@Component({
+  selector: 'app-region-list',
+  templateUrl: './region-list.component.html',
+  styleUrls: ['./region-list.component.scss']
+})
+export class RegionListComponent implements OnInit {
+
+  public regionsArray: Region[] = []
+
+  constructor( 
+    private regionServ: RegionsService 
+  ) { }
+
+  ngOnInit(): void { 
+    this.loadRegions();
+  }
+
+  loadRegions(){ 
+    this.regionServ.getRegions().subscribe({ 
+      next: regions => this.regionsArray = regions, 
+      error: err => console.log(err)
+    });
+  }
+}
---
+++ src/app/models/region.ts
@@ -0,0 +1,12 @@
+export interface Region { 
+    id: string, 
+    name: string, 
+    altSpelling: string, 
+    capital: string, 
+    flags: string, 
+    languages: any, 
+    maps: string, 
+    area: number, 
+    timezones: string, 
+    population: number
+}
